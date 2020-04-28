e0f3d8b115062a3ef2147f64c755429b5403ff45
Evgeny13166 <63262200+Evgeny13166@users.noreply.github.com>
2020-04-28 21:33:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+let bentley = {
+    carModel:"Bentayga I",
+    yearOfManufacture: 2016,
+    engineVolume: 6.0,
+    colorCar: "red",
+};
+let mercedes = {
+    carModel: "C-klasse",
+    yearOfManufacture: 2017,
+    engineVolume: 1.6,
+    colorCar: "gray",
+};
+let audi = {
+    carModel: "Q8",
+    yearOfManufacture: 2018,
+    engineVolume: 3.0,
+    colorCar: "gray",
+};
+let bmv = {
+    carModel:"X5",
+    yearOfManufacture: 2018,
+    engineVolume: 3.0,
+    colorCar: "black",
+};
+let cars = [bentley, mercedes, audi, bmv];
+console.log (cars);
